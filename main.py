3470c0edffaa0dca6fea3607cd0390abcf4fe322
Kyuuhachi <caagr98@gmail.com>
2023-09-24 11:14:03 +0200

    Improve error handling and exit codes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 import typing as T
-from sys import stderr
+from sys import stderr, exit
 import argparse
 from pathlib import Path
 from common import insn_table
@@ -12,7 +12,7 @@ argp.add_argument("-i", "--insn", help="path to instruction table", type = Path)
 argp.add_argument("-o", "--output", help="file or directory to place files in", type = Path)
 argp.add_argument("files", metavar="file", nargs="+", help="files to convert", type = Path)
 
-def __main__(quiet: bool, insn: Path | None, output: Path | None, files: list[Path]):
+def __main__(quiet: bool, insn: Path | None, output: Path | None, files: list[Path]) -> int:
 	make_output: T.Callable[[Path, str], Path]
 	if output is None:
 		make_output = lambda path, suffix: path.with_suffix(suffix)
@@ -24,24 +24,27 @@ def __main__(quiet: bool, insn: Path | None, output: Path | None, files: list[Pa
 
 	insns = insn_table(insn) if insn is not None else None
 
+	failed = False
 	for file in files:
 		if not quiet: print(f"{file} → ", file=stderr, end="", flush=True)
+		data = file.read_bytes()
 		if file.suffix == ".bin":
 			outfile = make_output(file, ".scp")
-			data = file.read_bytes()
 			scp = parse_bin.parse_ys7_scp(data, insns)
 			src = print_text.print_ys7_scp(scp)
-			outfile.write_text(src)
+			outdata = src.encode()
 		elif file.suffix == ".scp":
 			outfile = make_output(file, ".bin")
-			src = file.read_text()
+			src = data.decode()
 			scp = parse_text.parse_ys7_scp(src)
-			data = print_bin.write_ys7_scp(scp, insns)
-			outfile.write_bytes(data)
+			outdata = print_bin.write_ys7_scp(scp, insns)
 		else:
 			if not quiet: print(f"not sure how to handle", file=stderr)
+			failed = True
 			continue
 		if not quiet: print(f"{outfile}", file=stderr)
+		outfile.write_bytes(outdata)
+	return 0 if not failed else 2
 
 if __name__ == "__main__":
-	__main__(**argp.parse_args().__dict__)
+	exit(__main__(**argp.parse_args().__dict__))
